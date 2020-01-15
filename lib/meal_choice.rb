f20698433da102a519c67dbc6eb97f3833c49140
Valentin Placido <valentinplacido25@gmail.com>
2020-01-15 05:43:03 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@
 
 def meal_choice(veg1, veg2, protein = "meat")
   puts meal
-  return "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
 def meal
