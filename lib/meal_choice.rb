49fec0ab4c74b08830b7a10b121d20a2cdf8d78c
Valentin Placido <valentinplacido25@gmail.com>
2020-01-15 05:42:31 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,7 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice(veg1, veg2, protein = "meat")
-  meal
+  puts meal
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
